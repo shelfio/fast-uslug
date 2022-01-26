c949dc49d87aa64cbbfcfb7711043095ac69fcb0
Vlad Holubiev <golubev.vld@gmail.com>
2022-01-26 17:39:04 +0200

    feat: add benchmark script

---
+++ benchmark.ts
@@ -0,0 +1,40 @@
+import benny from 'benny';
+// @ts-ignore
+import fLI from 'fast-lorem-ipsum';
+// @ts-ignore
+import uslug from 'uslug';
+import {fastUslug} from './lib';
+
+const words10 = fLI(10, 'w');
+const words100 = fLI(100, 'w');
+const words1000 = fLI(1000, 'w');
+
+benny.suite(
+  '@shelf/fast-uslug',
+  benny.add('10 words', () => {
+    fastUslug(words10);
+  }),
+  benny.add('100 words', () => {
+    fastUslug(words100);
+  }),
+  benny.add('1000 words', () => {
+    fastUslug(words1000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'uslug',
+  benny.add('10 words', () => {
+    uslug(words10);
+  }),
+  benny.add('100 words', () => {
+    uslug(words100);
+  }),
+  benny.add('1000 words', () => {
+    uslug(words1000);
+  }),
+  benny.cycle(),
+  benny.complete()
+);
